a6aafd26a6639faedc442723f777d8851534ac5f
Telmen <telmenganhuyag@gmail.com>
2024-05-18 02:34:49 +0800

    trying out event object listener for the operand input

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-document.querySelector("")
+function add(a, b) { 
+    return a + b;
+}
+
+function subtract(a, b) { 
+    return a - b;
+}
+
+function multiply(a, b) { 
+    return a * b;   
+}
+
+function divide(a, b) { 
+    return a / b;
+}
+
+let operand;
+let operand2;
+let operator;
+
+function operate(operand, operator, operand2) { 
+    add();
+}
+
+const numberBtn = (event) => { 
+    const outputField = document.querySelector('#output');
+    const btn = event.target;
+    outputField.value += btn.innerText;
+};
+document.querySelectorAll('.operand').forEach((button) => button.addEventListener('click', numberBtn));
