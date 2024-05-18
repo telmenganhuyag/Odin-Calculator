1bed497c3eef1e63c82749db9dd19c3b22491a08
Telmen <telmenganhuyag@gmail.com>
2024-05-19 02:53:14 +0800

    actually so much complicated

--- script.js
+++ script.js
@@ -14,17 +14,55 @@ function divide(a, b) {
     return a / b;
 }
 
-let operand;
-let operand2;
-let operator;
+const display = document.querySelector('#display');
+const exec = display.innerText;
 
-function operate(operand, operator, operand2) { 
-    add();
+console.log(exec);
+
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+let result = null;
+
+function updateDisplay() { 
+    display.innerText = displayValue;
+    if (displayValue.length > 9) { 
+        display.innerText = displayValue.substring(0, 9);
+    }
 }
 
+updateDisplay();
+
+
+function operate (x, y, op) { 
+    if (op === '+') { 
+        return x + y;
+    } else if (op === '-') { 
+        return x - y;
+    } else if (op === '*') { 
+        return x * y;
+    } else if (op === '/') { 
+        if (y === '0') { 
+            return 'no';
+        } else return x / y;
+    }
+}
+
+
+
 const numberBtn = (event) => { 
-    const outputField = document.querySelector('#output');
     const btn = event.target;
-    outputField.value += btn.innerText;
+    display.innerText += btn.innerText;
+
 };
+
+// const operatorBtn = (event) => { 
+//     const op = event.target.innerText;
+//     exec = x;
+// };
+
+
 document.querySelectorAll('.operand').forEach((button) => button.addEventListener('click', numberBtn));
+// document.querySelectorAll('.operator').forEach((button) => button.addEventListener('click', operatorBtn));
+
